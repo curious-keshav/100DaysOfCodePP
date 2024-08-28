2dbd5cf429771fb8e6c6e27de3cf25ff5a50cccb
Keshav Verma <76473336+curious-keshav@users.noreply.github.com>
2024-08-29 01:44:46 +0530

    6th day

---
+++ 6_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        int low = 0;
+        int high = nums.size()-1;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){return mid;}
+
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target and target<=nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid]<=target and target<=nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
